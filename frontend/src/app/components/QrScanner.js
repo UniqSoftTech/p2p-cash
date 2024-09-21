a3a530c326b2bf8427705bd82d25e5430a153a99
ben0714 <john@uniqsoft.tech>
2024-09-21 19:15:03 +0800

    Add QrScanner

---
+++ frontend/src/app/components/QrScanner.js
@@ -0,0 +1,25 @@
+"use client";
+
+import React, { useState } from 'react';
+import { useZxing } from 'react-zxing';
+
+const QrScanner = () => {
+    const [result, setResult] = useState("");
+    const { ref } = useZxing({
+      onDecodeResult(result) {
+        setResult(result.getText());
+      },
+    });
+
+    return (
+        <>
+          <video ref={ref} />
+          <p>
+            <span>Last result:</span>
+            <span>{result}</span>
+          </p>
+        </>
+      );
+};
+
+export default QrScanner;
