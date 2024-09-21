5167849dbdb411ee0ddc20d7658039529c59f2d5
Jay <jiguur@uniqsoft.tech>
2024-09-22 00:15:32 +0800

    ui changes

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,17 +1,41 @@
 import dynamic from "next/dynamic";
+import VirtualCard from "./assets/virtual_card.png";
+import Background from "./assets/bg.jpg";
 import WalletConnect from "./components/ConnectWallet";
-
-const QrScanner = dynamic(() => import("./components/QrScanner"), {
-  ssr: false,
-});
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <QrScanner />
+    <div className="relative p-4 min-h-screen flex flex-col justify-between font-[family-name:var(--font-geist-sans)]">
+      <header className="rounded-2xl p-4 flex justify-between items-center z-10 relative">
+        <h1 className="text-white text-3xl font-bold">P2P Cash</h1>
+      </header>
+
+      <div className="relative flex-1 flex flex-col items-start justify-center z-10 px-16 gap-6">
+        <h2 className="text-white text-center text-4xl sm:text-5xl md:text-4xl font-bold">
+          P2P Payment Solution
+        </h2>
+        <p className="max-w-[550px]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+          vestibulum lacus non est laoreet, sit amet tempor lacus egestas. Proin
+          ut lacus at ante blandit maximus.
+        </p>
         <WalletConnect />
-      </main>
+      </div>
+
+      {/* Background Circle and Image */}
+      <div className="absolute inset-0 overflow-hidden">
+        <div className="bg-neutral-800 overflow-hidden h-[1000px] border-2 border-neutral-800/60 w-[1000px] absolute right-[-300px] top-[-200px] rounded-full">
+          <Image src={Background} className="w-full h-full" alt="bg" />
+        </div>
+        <Image
+          src={VirtualCard}
+          alt="Virtual Card"
+          width={500}
+          height={500}
+          className="absolute right-10 top-20 max-w-[90%] md:max-w-none"
+        />
+      </div>
     </div>
   );
 }
